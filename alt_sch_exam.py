cfd4d206aee585f64f24e9f0d165a3c495777ac8
Mayowa Akindaini <65491823+mayyurwha@users.noreply.github.com>
2023-12-23 10:30:44 +0200

    Update alt_sch_exam.py

--- alt_sch_exam.py
+++ alt_sch_exam.py
@@ -45,25 +45,3 @@ class ExpenseDatabase:
     def to_dict(self):
         return [expense.to_dict() for expense in self.expenses]
 
-# Example usage:
-
-# Create an ExpenseDatabase instance
-expense_db = ExpenseDatabase()
-
-# Add expenses to the database
-expense1 = Expense("Groceries", 50.0)
-expense2 = Expense("Rent", 1200.0)
-expense_db.add_expense(expense1)
-expense_db.add_expense(expense2)
-
-# Update an expense
-expense1.update(amount=60.0)
-
-# Get expenses by title
-grocery_expenses = expense_db.get_expense_by_title("Groceries")
-
-# Remove an expense
-expense_db.remove_expense(expense2.id)
-
-# Print the database as a list of dictionaries
-print(expense_db.to_dict())
